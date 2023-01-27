4c39aa21895ecf06c16696ee0e3a2e9295a29e20
Mathias Roustan <roustan_mathias@outlook.fr>
2023-01-27 21:03:33 +0100

    update to sveltekit stable

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,11 @@
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
-import path from 'path';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					$routes:			path.resolve('src/routes'),
-					$stores:			path.resolve('src/stores'),
-					$components:		path.resolve('src/components'),
-					$types:				path.resolve('src/types.ts'),
-					"$routes/*":		path.resolve('src/routes/*'),
-					"$stores/*":		path.resolve('src/stores/*'),
-					"$components/*":	path.resolve('src/components/*'),
-					"$types/*":			path.resolve('src/types/*'),
-				}
-			}
-		}
 	}
 };
 
